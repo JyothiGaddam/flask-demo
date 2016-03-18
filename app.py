06b099fad1438bf501e27693900e6e8e8b858aad
[Jyothi Gaddam] <[jneunaha@gmail.com]>
2016-03-19 06:58:48 +1300

    app.py changes

--- app.py
+++ app.py
@@ -12,10 +12,34 @@ def index():
 
 @app.route('/graph', methods = ['POST'])
 def graph():
-  print(request.form.get('stock',none))
-  print( request.form.get('tickerdetails',None),"checked")
-  return render_template('graph.html')
+    print(request.form.get('stock',none))
+    print( request.form.get('tickerdetails',None),"checked")
+    resp_data = getdata(request.form.get('stock'))
+    df = getdf(resp_data)
+    df.plot = figure(tools=TOOLS,
+	      title='Data from Quandl WIKI set',
+	      x_axis_label='date',
+	      x_axis_type='datetime')
 
+     script, div = components(plot)
 
+     return render_template('graph.html', script=script, div=div)
+    
+
+def getdata(stock):
+    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
+    session = re.Session()
+    session.mount('http://',re.adapters.HTTPAdapter(max_retries=3))
+    raw_data = session.get(api_url)
+    return raw_data
+
+def getdf(resp_data):
+     j = resp_data.json()
+     df = pd.DataFrame(j["data"])
+     df.columns = j["column_names"]
+     return df 
+ 
+       
+       
 if __name__ == '__main__':
   app.run(port=33507)
