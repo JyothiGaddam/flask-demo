d1d18f9ca367f62baace4e113f5255b8097a0d75
[Jyothi Gaddam] <[jneunaha@gmail.com]>
2016-03-20 07:28:51 +1300

    testing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
+import numpy as np
 from bokeh.plotting import figure
 import jinja2
 from bokeh.embed import components
@@ -15,57 +16,31 @@ def main():
 def index():
   return render_template('index.html')
 
-@app.route('/analysis',methods = ['POST'])
-def analysis():
-    stock = request.form['stock']
-    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
-    session = requests.Session()
-    session.mount('http://',requests.adapters.HTTPAdapter(max_retries=3))
-    resp_data = session.get(api_url)
-    #resp_data = getdata(stock)
-    j = resp_data.json()
-    df = pd.DataFrame(j["data"])
-    df.columns = j["column_names"]
-    x_list = df['Date'][:3]
-    y_list = df['Close'][:3]
-    plot = figure(title='Data from Quandl WIKI set',x_axis_label = 'date',x_axis_type='datetime')
-    plot.line(x_list,y_list, color='#A6CEE3', legend=stock)
-    
-    plot.yaxis.axis_label = 'Price' 
-    plot.legend.orientation = "top_left"
-    script, div = components(plot)
-    
-    #return render_template('graph.html', script=script, div=div)
-    return render_template("analysis.html", script=script, div=div)
-                                                                
+                                                               
 
 @app.route('/graph', methods = ['POST'])
 def graph():
     
     stock = request.form['stock']
-    #print stock
-    #plttype = request.form.tickerdetails
-    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
-    session = requests.Session()
-    session.mount('http://',requests.adapters.HTTPAdapter(max_retries=3))
-    resp_data = session.get(api_url)
-    #resp_data = getdata(stock)
-    j = resp_data.json()
-    df = pd.DataFrame(j["data"])
-    df.columns = j["column_names"]
+    resp_data = getdata(stock)
+    df = getdf(resp_data)
     
-    df["Date"] = pd.to_datetime(df["Date"])
-    y_list = [10,20,30,40,50,60,70,80,90]
     TOOLS = 'box_zoom,box_select,resize,reset,hover,wheel_zoom'
-    plot = figure(tools=TOOLS,title='Data from Quandl WIKI set',x_axis_label='Date',x_axis_type='datetime')
-    plot.line(x_list, y_list, color='#A6CEE3', legend=stock)
+    p1 = figure(tools=TOOLS, title='Data from Quandl WIKI set',x_axis_type = "datetime")
+    #p1.title = "Stock Closing Prices"
+    p1.grid.grid_line_alpha=0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
+
+    p1.line(datetime(df['Date']), df['Close'], color='#A6CEE3', legend=stock)
     
-    plot.yaxis.axis_label = 'Price' 
-    plot.legend.orientation = "top_left"
-    script, div = components(plot)
+    script, div = components(p1)
     
     return render_template('graph.html', script=script, div=div)
-    
+
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
+
 
 def getdata(stock):
     api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
