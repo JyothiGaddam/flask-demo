810a13099a88f163624871aad92936ca4c3a0183
[Jyothi Gaddam] <[jneunaha@gmail.com]>
2016-03-19 21:13:02 +1300

    testing

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def analysis():
     j = resp_data.json()
     df = pd.DataFrame(j["data"])
     df.columns = j["column_names"]
-    return render_template("analysis.html", mystring=stock, my_list=df.columns)
+    return render_template("analysis.html", mystring=stock, my_list=df['Date'])
                                                                 
 """
 @app.route('/graph', methods = ['POST'])
