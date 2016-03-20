8bb20028d121b2fd73eac7248c41773d63fccab6
[Jyothi Gaddam] <[jneunaha@gmail.com]>
2016-03-20 16:09:35 +1300

    test

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def datetime(x):
 def graph():
     
     stock = request.form['stock']
-    resp_str = request.POST('tickerdetails')
+    resp_str = request.form('tickerdetails')
     #ytypes = resp_str.split(",")
     api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
     session = requests.Session()
